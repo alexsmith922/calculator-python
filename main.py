ebcd925420ee57b4a99ab01ef5d74d6d5da812cb
Alex Smith <54478329+alexsmith922@users.noreply.github.com>
2021-09-23 11:10:32 +0000

    Condensed code spacing, added calculator() recursion

--- main.py
+++ main.py
@@ -21,35 +21,24 @@ operations = {
   "/" : divide,
 }
 
-num1 = int(input("What's the first number?: "))
-
-for symbol in operations:
-  print(symbol)
-
-operation_symbol = input("Pick an operation from the line above: ")
-
-num2 = int(input("What's the second number?: "))
-
-calculation_function = operations[operation_symbol]
-first_answer = calculation_function(num1, num2)
-
-print(f"{num1} {operation_symbol} {num2} = {first_answer}")
-
-operation_symbol = input("Pick an operation from the line above: ")
-num3 = int(input("What's the next number?: "))
-
-calculation_function = operations[operation_symbol]
-second_answer = calculation_function(first_answer, num3)
-print(f"{first_answer} {operation_symbol} {num3} = {second_answer}")
-
-clear_choice = input("Type 'y' to continue calculating with 16, or type 'n' to exit.: ")
-
-if clear_choice == "y":
-  num1 = int(input("What's the next number?: "))
+def calculator():
+  num1 = int(input("What's the first number?: "))
   for symbol in operations:
     print(symbol)
-  num2= int(input("What's the second number?: "))
-  calculation_function = operations[operation_symbol]
-  first_answer = calculation_function(num1, num2)
-else:
-  return
+  should_continue = True
+
+  while should_continue:
+    operation_symbol = input("Pick an operation from the line above: ")
+    num2 = int(input("What's the second number?: "))
+    calculation_function = operations[operation_symbol]
+    answer = calculation_function(num1, num2)
+
+    print(f"{num1} {operation_symbol} {num2} = {answer}")
+
+    if input(f"Type 'y' to to continue calculating with {answer}: ") == "y": 
+      num1 == answer
+    else:
+      should_continue = False
+      calculator()
+
+calculator()
