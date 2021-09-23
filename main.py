487b885d597bbb25aa5939a8e4702585ad7a0c20
Alex Smith <54478329+alexsmith922@users.noreply.github.com>
2021-09-23 11:17:31 +0000

    changed int inputs to float

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+from art import logo
+
 #add
 def add(n1, n2):
   return n1 + n2
@@ -22,14 +24,16 @@ operations = {
 }
 
 def calculator():
-  num1 = int(input("What's the first number?: "))
+  print(logo)
+
+  num1 = float(input("What's the first number?: "))
   for symbol in operations:
     print(symbol)
   should_continue = True
 
   while should_continue:
     operation_symbol = input("Pick an operation from the line above: ")
-    num2 = int(input("What's the second number?: "))
+    num2 = float(input("What's the second number?: "))
     calculation_function = operations[operation_symbol]
     answer = calculation_function(num1, num2)
 
